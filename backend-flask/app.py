6003e35636ebd86b4b694b7924d5f4c31496a14e
Kaydee406 <kanudavid.c@gmail.com>
2023-04-28 18:16:58 +0000

    Revert "row commit"
    This reverts commit 975944626258ff6399ae96d39fc8fde74a78004b.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -105,8 +105,9 @@ cors = CORS(
 
 # Rollbar ----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
+#@app.before_first_request
+with app.app_context():
+  def init_rollbar():
     """init rollbar module"""
     rollbar.init(
         # access token
@@ -128,7 +129,7 @@ def rollbar_test():
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
-  user_handle  = 'kaydee6991'
+  user_handle  = 'andrewbrown'
   model = MessageGroups.run(user_handle=user_handle)
   if model['errors'] is not None:
     return model['errors'], 422
@@ -137,7 +138,7 @@ def data_message_groups():
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
-  user_sender_handle = 'kaydee6991'
+  user_sender_handle = 'andrewbrown'
   user_receiver_handle = request.args.get('user_reciever_handle')
 
   model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
@@ -150,7 +151,7 @@ def data_messages(handle):
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-  user_sender_handle = 'kaydee6991'
+  user_sender_handle = 'andrewbrown'
   user_receiver_handle = request.json['user_receiver_handle']
   message = request.json['message']
 
@@ -206,7 +207,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'kaydee6991'
+  user_handle  = 'andrewbrown'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
@@ -225,7 +226,7 @@ def data_show_activity(activity_uuid):
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities_reply(activity_uuid):
-  user_handle  = 'kaydee6991'
+  user_handle  = 'andrewbrown'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
   if model['errors'] is not None:
--- backend-flask/services/create_message.py
+++ backend-flask/services/create_message.py
@@ -20,7 +20,7 @@ class CreateMessage:
     if model['errors']:
       # return what we provided
       model['data'] = {
-        'display_name': 'Kanu David',
+        'display_name': 'Andrew Brown',
         'handle':  user_sender_handle,
         'message': message
       }
@@ -28,7 +28,7 @@ class CreateMessage:
       now = datetime.now(timezone.utc).astimezone()
       model['data'] = {
         'uuid': uuid.uuid4(),
-        'display_name': 'Kanu David',
+        'display_name': 'Andrew Brown',
         'handle':  user_sender_handle,
         'message': message,
         'created_at': now.isoformat()
